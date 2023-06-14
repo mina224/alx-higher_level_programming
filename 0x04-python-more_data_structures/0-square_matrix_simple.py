fd6479be6bb9da391ae1ef18541be21269bf1d4a
mina224 <salimmina027@gmail.com>
2023-06-14 23:12:50 +0000

    data structrue python

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    for row in matrix:
+        new_row = [nbr ** 2 for nbr in row]
+        new_matrix.append(new_row)
+    return new_matrix
