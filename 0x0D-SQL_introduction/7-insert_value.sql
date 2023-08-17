0092baec5177cb488315e66c0cffe111ba47d099
mina224 <salimmina027@gmail.com>
2023-08-17 16:24:00 -0700

    0x0D-SQL_introduction

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,3 @@
--- Insert to a table
-INSERT INTO `first_table` (`id`, `name`) VALUES ('89', "BEST SCHOOL");
+-- inserts a new row in table first_table
+-- in database hbtn_0c_0
+INSERT INTO first_table(id, name) VALUES (89, 'Best School');
